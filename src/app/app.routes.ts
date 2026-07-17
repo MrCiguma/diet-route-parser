e880d960e0ffa80e9318a2ba9c1c92d62d52fe1c
mrajterMK <miha.rajter@metakocka.si>
2026-07-17 08:12:42 +0200

    route-creator added: a page that takes as parameters the coordinates/merchant data of your account and returns how exactly to set up diet routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DietRouteParserComponent } from './diet-route-parser/diet-route-parser.component';
+import { RouteCreatorComponent } from './route-creator/route-creator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DietRouteParserComponent },
+  { path: 'route-creator', component: RouteCreatorComponent },
+];
